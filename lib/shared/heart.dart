3e1391fa2dafdcc391f24a7567316ea8e621848e
Asnan Sultan <ansn.sultan676@gmail.com>
2021-09-11 10:23:18 +0500

    Day3 completed (Animation Controller)

--- lib/shared/heart.dart
+++ lib/shared/heart.dart
@@ -5,16 +5,70 @@ class Heart extends StatefulWidget {
   _HeartState createState() => _HeartState();
 }
 
-class _HeartState extends State<Heart> {
+class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  Animation _colorAnimation;
+  Animation<double> _sizedAnimation;
+  bool isRed = false;
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: Duration(milliseconds: 300),
+      vsync: this,
+    );
+
+    _colorAnimation = ColorTween(begin: Colors.grey[400], end: Colors.red).animate(_controller);
+
+    _sizedAnimation = TweenSequence(<TweenSequenceItem<double>>[
+      TweenSequenceItem<double>(
+        tween: Tween<double>(begin: 30, end: 50),
+        weight: 50,
+      ),
+      TweenSequenceItem<double>(
+        tween: Tween<double>(begin: 50, end: 30),
+        weight: 50,
+      ),
+    ]).animate(_controller);
+    _controller.addListener(() {
+      print(_controller.value);
+    });
+    _controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        setState(() {
+          isRed = true;
+        });
+      }
+      if (status == AnimationStatus.dismissed) {
+        setState(() {
+          isRed = false;
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      icon: Icon(
-        Icons.favorite,
-        color: Colors.grey[400],
-        size: 30,
-      ),
-      onPressed: () {},
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (BuildContext context, _) {
+        return IconButton(
+          icon: Icon(
+            Icons.favorite,
+            color: _colorAnimation.value,
+            size: _sizedAnimation.value,
+          ),
+          onPressed: () {
+            isRed ? _controller.reverse() : _controller.forward();
+          },
+        );
+      },
     );
   }
 }
